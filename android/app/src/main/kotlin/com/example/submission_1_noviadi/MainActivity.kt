5d3987f93070b9f548002572297b0da3d9825c5f
Novi Adi Triswandi <icambing@gmail.com>
2022-09-27 11:32:15 +0700

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/submission_1_noviadi/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.submission_1_noviadi
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Dicoding Course App',
+      theme: ThemeData(),
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: EdgeInsets.only(left: 20, top: 50, right: 20),
+        child: Column(
+          children: <Widget>[
+            Row(
+              children: <Widget>[SvgPicture.asset("assets/icons/menu.svg")],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
